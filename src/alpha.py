00f3b398decf75eb49aee3909b903eb6df849dd9
JeffRisberg <j.s.risberg@gmail.com>
2024-07-25 14:30:56 -0700

    added another example of a password

--- src/alpha.py
+++ src/alpha.py
@@ -5,6 +5,38 @@ alpha = "AbB-8455-dfdfdf-6565656-343434"
 print(alpha)
 
 
+token = "AbB-8455-dfdfdf-6565656-343434"
+
+print(token)
+
+
 password = "AbB-8455-dfdfdf-6565656-343434"
 
 print(password)
+
+
+config_dict1 = {
+    "name":  "John Smith",
+    "type":  "employee",
+    "alpha": "dfddf-dfdfdf-efefefe-4656gd-333"
+}
+
+print(config_dict1)
+
+
+config_dict2 = {
+    "name":  "John Smith",
+    "type":  "employee",
+    "token": "dfddf-dfdfdf-efefefe-4656gd-333"
+}
+
+print(config_dict2)
+
+
+config_dict3 = {
+    "name":  "John Smith",
+    "type":  "employee",
+    "password": "dfddf-dfdfdf-efefefe-4656gd-333"
+}
+
+print(config_dict3)
